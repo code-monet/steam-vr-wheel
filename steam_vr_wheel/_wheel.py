4cca3b8825d091b8c0fe4e54183c44528947d01a
hjjg200 <hjjg200@gmail.com>
2024-04-17 18:51:59 +0900

    Remove other orders from rotation matrix

--- steam_vr_wheel/_wheel.py
+++ steam_vr_wheel/_wheel.py
@@ -37,15 +37,8 @@ def rotation_matrix_around_vec(theta, vec):
                     [uy*ux*(1-c)+uz*s, c+uy**2*(1-c), uy*uz*(1-c)-ux*s],
                     [uz*ux*(1-c)-uy*s, uz*uy*(1-c)+ux*s, c+uz**2*(1-c)]])
 
-def rotation_matrix(theta1, theta2, theta3, order='xyz'):
+def rotation_matrix(theta1, theta2, theta3):
     #https://programming-surgeon.com/en/euler-angle-python-en/
-    """
-    input
-        theta1, theta2, theta3 = rotation angles in rotation order (degrees)
-        oreder = rotation order of x,y,z　e.g. XZY rotation -- 'xzy'
-    output
-        3x3 rotation matrix (numpy array)
-    """
     c1 = np.cos(theta1 * np.pi / 180)
     s1 = np.sin(theta1 * np.pi / 180)
     c2 = np.cos(theta2 * np.pi / 180)
@@ -53,56 +46,9 @@ def rotation_matrix(theta1, theta2, theta3, order='xyz'):
     c3 = np.cos(theta3 * np.pi / 180)
     s3 = np.sin(theta3 * np.pi / 180)
 
-    if order == 'xzx':
-        matrix=np.array([[c2, -c3*s2, s2*s3],
-                         [c1*s2, c1*c2*c3-s1*s3, -c3*s1-c1*c2*s3],
-                         [s1*s2, c1*s3+c2*c3*s1, c1*c3-c2*s1*s3]])
-    elif order=='xyx':
-        matrix=np.array([[c2, s2*s3, c3*s2],
-                         [s1*s2, c1*c3-c2*s1*s3, -c1*s3-c2*c3*s1],
-                         [-c1*s2, c3*s1+c1*c2*s3, c1*c2*c3-s1*s3]])
-    elif order=='yxy':
-        matrix=np.array([[c1*c3-c2*s1*s3, s1*s2, c1*s3+c2*c3*s1],
-                         [s2*s3, c2, -c3*s2],
-                         [-c3*s1-c1*c2*s3, c1*s2, c1*c2*c3-s1*s3]])
-    elif order=='yzy':
-        matrix=np.array([[c1*c2*c3-s1*s3, -c1*s2, c3*s1+c1*c2*s3],
-                         [c3*s2, c2, s2*s3],
-                         [-c1*s3-c2*c3*s1, s1*s2, c1*c3-c2*s1*s3]])
-    elif order=='zyz':
-        matrix=np.array([[c1*c2*c3-s1*s3, -c3*s1-c1*c2*s3, c1*s2],
-                         [c1*s3+c2*c3*s1, c1*c3-c2*s1*s3, s1*s2],
-                         [-c3*s2, s2*s3, c2]])
-    elif order=='zxz':
-        matrix=np.array([[c1*c3-c2*s1*s3, -c1*s3-c2*c3*s1, s1*s2],
-                         [c3*s1+c1*c2*s3, c1*c2*c3-s1*s3, -c1*s2],
-                         [s2*s3, c3*s2, c2]])
-    elif order=='xyz':
-        matrix=np.array([[c2*c3, -c2*s3, s2],
+    return np.array([[c2*c3, -c2*s3, s2],
                          [c1*s3+c3*s1*s2, c1*c3-s1*s2*s3, -c2*s1],
                          [s1*s3-c1*c3*s2, c3*s1+c1*s2*s3, c1*c2]])
-    elif order=='xzy':
-        matrix=np.array([[c2*c3, -s2, c2*s3],
-                         [s1*s3+c1*c3*s2, c1*c2, c1*s2*s3-c3*s1],
-                         [c3*s1*s2-c1*s3, c2*s1, c1*c3+s1*s2*s3]])
-    elif order=='yxz':
-        matrix=np.array([[c1*c3+s1*s2*s3, c3*s1*s2-c1*s3, c2*s1],
-                         [c2*s3, c2*c3, -s2],
-                         [c1*s2*s3-c3*s1, c1*c3*s2+s1*s3, c1*c2]])
-    elif order=='yzx':
-        matrix=np.array([[c1*c2, s1*s3-c1*c3*s2, c3*s1+c1*s2*s3],
-                         [s2, c2*c3, -c2*s3],
-                         [-c2*s1, c1*s3+c3*s1*s2, c1*c3-s1*s2*s3]])
-    elif order=='zyx':
-        matrix=np.array([[c1*c2, c1*s2*s3-c3*s1, s1*s3+c1*c3*s2],
-                         [c2*s1, c1*c3+s1*s2*s3, c3*s1*s2-c1*s3],
-                         [-s2, c2*s3, c2*c3]])
-    elif order=='zxy':
-        matrix=np.array([[c1*c3-s1*s2*s3, -c2*s1, c1*s3+c3*s1*s2],
-                         [c3*s1+c1*s2*s3, c1*c2, s1*s3-c1*c3*s2],
-                         [-c2*s3, s2, c2*c3]])
-
-    return matrix
 
 def initRotationMatrix(axis, angle, matrix=None):
     # angle in radians
